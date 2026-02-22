12dc115c2458b13f0b48934a9110e4398092f41b
Kotfil <filippkotenko@outlook.com>
2026-02-22 03:54:18 +0200

    feat: add variables

---
+++ ch01/ex1_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+    int v1 = 10, v2 = 20;
+
+
+    std::cout << "The sum of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 + v2;
+    std::cout << std::endl;
+
+    return 0;
+}
